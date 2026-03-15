64ec851da2ee2c0b48c5bd10c3771d34329f33f8
rockybanna <ajayrajpoot420@gmail.com>
2026-03-15 17:12:14 +0530

    Add Supabase global client

--- shared/js/supabase-client.js
+++ shared/js/supabase-client.js
@@ -1,8 +1,8 @@
 // Supabase Global Client
 
-import { createClient } from "https://uouwizrknwrqqynodplv.supabase.co";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
+const SUPABASE_URL = "https://uouwizrknwrqqynodplv.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvdXdpenJrbndycXF5bm9kcGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNDk1MjksImV4cCI6MjA4ODYyNTUyOX0.oxDZqaT1bMed6uuirN2ZoDCambPYLhOHp2PlhX8gaco";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
