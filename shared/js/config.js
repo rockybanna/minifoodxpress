e3fe43192874cb7adc18659949047e868ec9d9ef
rockybanna <ajayrajpoot420@gmail.com>
2026-03-15 17:51:35 +0530

    Update config.js

--- shared/js/config.js
+++ shared/js/config.js
@@ -1,9 +1,19 @@
-// Supabase Global Client
+export const CONFIG = {
 
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { CONFIG } from "./config.js";
+  APP_NAME: "MiniFoodXpress",
 
-export const supabase = createClient(
-  CONFIG.SUPABASE_URL,
-  CONFIG.SUPABASE_ANON_KEY
-);
+  SUPABASE_URL: "https://uouwizrknwrqqynodplv.supabase.co",
+
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvdXdpenJrbndycXF5bm9kcGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNDk1MjksImV4cCI6MjA4ODYyNTUyOX0.oxDZqaT1bMed6uuirN2ZoDCambPYLhOHp2PlhX8gaco",
+
+  MOV: 149,
+
+  MAX_ORDER_VALUE: 5000,
+
+  MAX_DP_ORDERS: 2,
+
+  MAX_WAITING_ORDERS: 3,
+
+  DP_RATE_PER_KM: 10
+
+};
